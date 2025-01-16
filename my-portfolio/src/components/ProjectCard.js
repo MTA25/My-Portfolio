df2933bca4f6160fb0d416c128632ed38a4b4934
umair.siddique16@gmail.com <umair.siddique164@gmail.com>
2025-01-16 23:04:55 +0500

    Changed skills UI

--- my-portfolio/src/components/ProjectCard.js
+++ my-portfolio/src/components/ProjectCard.js
@@ -1,9 +1,9 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, onClick }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
+      <div className="proj-imgbx" onClick={onClick}>
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -1,12 +1,8 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCameraRetro, faCode, faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons'
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
@@ -38,20 +34,16 @@ export const Skills = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <FontAwesomeIcon fontSize={30} icon={faCode} />
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <FontAwesomeIcon fontSize={30} icon={faCameraRetro} />
                                 <h5>Brand Identity</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <FontAwesomeIcon fontSize={30} icon={faWandMagicSparkles} />
                                 <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
                             </div>
                         </Carousel>
                     </div>
