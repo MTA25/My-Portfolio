5aed8ac44d1699fc5379cf0066dc0849a261279a
Hadid <talha1248@gmail.com>
2025-02-15 23:39:53 +0500

    Add new projects data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -67,7 +67,64 @@ import CowboyShooting3 from "../assets/projectImages/CowboyShooting1.png";
 import CowboyShooting4 from "../assets/projectImages/CowboyShooting3.png";
 import CowboyShooting5 from "../assets/projectImages/CowboyShooting4.png";
 import CowboyShooting6 from "../assets/projectImages/CowboyShooting5.png";
-import CowboyShootingVideo from "../assets/projectImages/DontTouch-video.mp4";
+import DontTouchVideo from "../assets/projectImages/DontTouch-video.mp4";
+import DontTouchIcon from "../assets/projectImages/DontTouchIcon.png";
+import DontTouch1 from "../assets/projectImages/DontTouch1.png";
+import DontTouch2 from "../assets/projectImages/DontTouch2.png";
+import DontTouch3 from "../assets/projectImages/DontTouch3.png";
+import DontTouch4 from "../assets/projectImages/DontTouch4.png";
+import USPoliceDogCityCrimeChase1 from "../assets/projectImages/US Police Dog City Crime Chase (1).png";
+import USPoliceDogCityCrimeChase2 from "../assets/projectImages/US Police Dog City Crime Chase (2).png";
+import USPoliceDogCityCrimeChase3 from "../assets/projectImages/US Police Dog City Crime Chase (3).png";
+import Daleela1 from "../assets/projectImages/Daleela (1).jpg";
+import Daleela2 from "../assets/projectImages/Daleela (2).jpg";
+import Daleela3 from "../assets/projectImages/Daleela (3).jpg";
+import Daleela4 from "../assets/projectImages/Daleela (4).jpg";
+import Daleela5 from "../assets/projectImages/Daleela (5).jpg";
+import Daleela6 from "../assets/projectImages/Daleela (6).jpg";
+import Daleela7 from "../assets/projectImages/Daleela (7).jpg";
+import Daleela8 from "../assets/projectImages/Daleela (8).jpg";
+import Daleela9 from "../assets/projectImages/Daleela (9).jpg";
+import Daleela10 from "../assets/projectImages/Daleela (10).jpg";
+import Daleela11 from "../assets/projectImages/Daleela (11).jpg";
+import Daleela12 from "../assets/projectImages/Daleela (12).jpg";
+import Daleela13 from "../assets/projectImages/Daleela (13).jpg";
+import SupperDelivery0 from "../assets/projectImages/demo-superdelivery (icon).jpg";
+import SupperDelivery1 from "../assets/projectImages/demo-superdelivery (1).jpg";
+import SupperDelivery2 from "../assets/projectImages/demo-superdelivery (2).jpg";
+import SupperDelivery3 from "../assets/projectImages/demo-superdelivery (3).jpg";
+import SupperDeliveryVideo from "../assets/projectImages/demo-superdelivery.mp4";
+import Bimparkour from "../assets/projectImages/BIMParkor0 .png";
+import Bimparkour1 from "../assets/projectImages/BIMParkor (1).jpg";
+import Bimparkour2 from "../assets/projectImages/BIMParkor (2).jpg";
+import Bimparkour3 from "../assets/projectImages/BIMParkor (3).jpg";
+import Bimparkour4 from "../assets/projectImages/BIMParkor (4).jpg";
+import Bimparkour5 from "../assets/projectImages/BIMParkor (5).jpg";
+import Bimparkour6 from "../assets/projectImages/BIMParkor (6).jpg";
+import Bimparkour7 from "../assets/projectImages/BIMParkor (7).jpg";
+import Bimparkour8 from "../assets/projectImages/BIMParkor (8).jpg";
+import BimparkourVideo from "../assets/projectImages/parkourdemo.mp4";
+import Paratroopers0 from "../assets/projectImages/Paratroopers0.png";
+import Paratroopers1 from "../assets/projectImages/Paratroopers1.png";
+import Paratroopers2 from "../assets/projectImages/Paratroopers2.png";
+import Paratroopers3 from "../assets/projectImages/Paratroopers3.png";
+import Paratroopers4 from "../assets/projectImages/Paratroopers4.png";
+import ParatroopersVideo from "../assets/projectImages/ParatroopersVideo.mp4";
+import MakeWayVideo from "../assets/projectImages/ParatroopersVideo.mp4";
+import MakeWay0 from "../assets/projectImages/MakeWayIcon.png";
+import MakeWay1 from "../assets/projectImages/MakeWay1.png";
+import MakeWay2 from "../assets/projectImages/MakeWay2.png";
+import MakeWay3 from "../assets/projectImages/MakeWay3.png";
+import MakeWayRep1 from "../assets/projectImages/fliptiles1.jpg";
+import MakeWayRep2 from "../assets/projectImages/fliptiles2.jpg";
+import MakeWayRep3 from "../assets/projectImages/fliptiles3.jpg";
+import MakeWayRep4 from "../assets/projectImages/fliptiles4.jpg";
+import MakeWayRep5 from "../assets/projectImages/fliptiles5.jpg";
+import OhMyDice0 from "../assets/projectImages/OhMyDice (0).jpg";
+import OhMyDice1 from "../assets/projectImages/OhMyDice (1).jpg";
+import OhMyDice2 from "../assets/projectImages/OhMyDice (2).jpg";
+import OhMyDice3 from "../assets/projectImages/OhMyDice (3).jpg";
+import OhMyDicevideo from "../assets/projectImages/ohmydicedemo.mp4";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   "& .MuiDialogContent-root": {
@@ -125,52 +182,82 @@ export const Projects = () => {
   const projects = [
     {
       title: "Azure Dungeon Survivor (Computer Game)",
-      description:
-        "The Bug Squasher Minigame uses a modular architecture to manage bug spawning, behavior, " +
-        "and gameplay mechanics through controllers and configurations.",
-      link: "https://github.com/",
+      description:"Azure Dungeon Survivor is a strategic grid-based, turn-based PC game where players attack first, followed by all enemies. Plan each move carefully, adapt to enemy tactics, and survive waves of dungeon threats. With challenging encounters and tactical combat, every decision matters in this battle for survival!",
+      link: null,
       imgUrl: Azure5,
-      images: [Azure4, Azure3, Azure1, CowboyShootingVideo],
+      images: [Azure5, Azure4, Azure3, Azure1],
     },
     {
       title: "Top Burger (Mobile Game)",
-      description: "",
+      description: "Top Burger is a fast-paced 2D burger-making simulation game where you serve delicious meals across multiple levels. Take orders, stack ingredients quickly, and satisfy hungry customers to earn rewards. With increasing difficulty and fun challenges, test your speed and accuracy to become the ultimate burger chef!",
       link: null,
-      imgUrl: burgermaking3,
-      images: [burgermaking1, burgermaking2, burgermaking4, burgermaking5],
+      imgUrl: burgermaking1,
+      images: [burgermaking1, burgermaking2, burgermaking3, burgermaking4, burgermaking5],
+    },
+    {
+      title: "Daleela (Mobile Game)",
+      description: "Daleela is an educational health app by UNICEF designed to provide vital maternal and child health information. It offers expert guidance on pregnancy, newborn care, and early childhood development, helping parents make informed decisions for their child’s well-being.",
+      link: "https://play.google.com/store/apps/details?id=com.unicef.daleela&hl=en",
+      imgUrl: Daleela1,
+      images: [Daleela1, Daleela2, Daleela3, Daleela4, Daleela5, Daleela6, Daleela7, Daleela8, Daleela9, Daleela10, Daleela11, Daleela12, Daleela13],
     },
     {
       title: "Funny Shooter 2 (WebGL Game)",
-      description: "",
-      link: null,
+      description: "Funny Shooter 2 is a fast-paced FPS where you battle wacky, absurd enemies in chaotic arenas. Armed with a wild arsenal of weapons, you take on bizarre foes like giant toilets and crazy soldiers. With smooth controls, explosive combat, and hilarious action, this game delivers non-stop fun and mayhem!",
+      link: "https://poki.com/en/g/funny-shooter-2",
       imgUrl: funnyShooter1,
-      images: [funnyShooter2, funnyShooter3, funnyShooter4],
+      images: [funnyShooter1, funnyShooter2, funnyShooter3, funnyShooter4],
     },
     {
       title: "3D Car Parking (Mobile Game)",
-      description: "",
-      link: "https://www.linkedin.com/",
+      description: "3D Car Parking is a realistic mobile driving game that tests your parking skills across challenging levels. Navigate tight spaces, avoid obstacles, and master precision parking with smooth controls and lifelike physics.",
+      link: null,
       imgUrl: carparking1,
-      images: [carparking2, carparking3, carparking4, carparking5],
+      images: [carparking1, carparking2, carparking3, carparking4, carparking5],
     },
     {
       title: "Toon Car Parking 3D (Mobile Game)",
-      description: "",
+      description: "Toon Car Parking 3D is a fun and colorful parking simulation game with cartoon-style graphics. Drive through challenging levels, navigate tricky obstacles, and park with precision. With smooth controls and engaging gameplay.",
       link: null,
       imgUrl: drcarparking1,
-      images: [drcarparking2, drcarparking3, drcarparking4],
+      images: [drcarparking1, drcarparking2, drcarparking3, drcarparking4],
+    },
+    {
+      title: "Super Delivery (Mobile Game)",
+      description: "A hypercasual project Demo made in collaboration with Reknown Hypercasual Publishers ( Cant reveal identity due to NDA Clauses). Game Involves doing deliveries of the products as a delivery boy.",
+      link: null,
+      imgUrl: SupperDelivery0,
+      images: [SupperDelivery1, SupperDelivery2, SupperDelivery3, SupperDeliveryVideo],
+    },
+    {
+      title: "Oh My Dice (Mobile Game)",
+      description: "Oh My Dice is a puzzle grid-based game where players roll dice strategically to ensure all dice show the same top color. Solve each level by carefully planning your moves and aligning the dice correctly.",
+      link: null,
+      imgUrl: OhMyDice0,
+      images: [OhMyDice1, OhMyDice2, OhMyDice3, OhMyDicevideo],
+    },
+    {
+      title: "Parkour Race: Jungle Runner 3D (Mobile Game)",
+      description: "Jungle Parkour Runner is a fast-paced running game where you dash through wild jungle environments, overcoming obstacles with fluid parkour moves. Jump, slide, and sprint to escape dangers while collecting rewards. With exciting levels and smooth controls, it's a thrilling adventure for all runners!",
+      link: "https://play.google.com/store/apps/details?id=com.virtua.play.parkour.runner.jungle.race.game&hl=en",
+      imgUrl: Bimparkour,
+      images: [Bimparkour7, Bimparkour1, Bimparkour, Bimparkour3, Bimparkour4, Bimparkour5, Bimparkour6, Bimparkour8, BimparkourVideo],
     },
     {
       title: "FPS Shooter (Mobile Game)",
-      description: "",
+      description: "FPS Shooter is an action-packed mobile game featuring intense gunfights, realistic weapons, and fast-paced combat. Take on challenging missions, eliminate enemies, and test your shooting skills in immersive battle environments. Lock, load, and dominate the battlefield!",
       link: null,
       imgUrl: fpsShooter1,
-      images: [fpsShooter2, fpsShooter3, fpsShooter4, fpsShooter5],
+      images: [fpsShooter1, fpsShooter2, fpsShooter3, fpsShooter4, fpsShooter5],
     },
     {
-      title: "Cowboy Shooting (Mobile Game)",
-      description: "",
-      link: null,
+      title: "West World Cowboy Shooter (Mobile Game)",
+      description: "Mafia Shooting Chronicles is an intense cover shooter where players battle ruthless crime syndicates across five action-packed chapters. " +
+          "From New York’s gritty streets to Tokyo’s neon-lit underworld, they take down notorious mafia bosses, including the Chinese triads, " +
+          "Russian mob, Yakuza, and a powerful South American cartel. Featuring dynamic cover mechanics, a vast arsenal, and a skill tree for " +
+          "customization, the game blends strategy with fast-paced combat. Stunning noir-inspired visuals and cinematic sequences immerse players" +
+          " in a high-stakes fight for underworld dominance.",
+      link: "https://play.google.com/store/apps/details?id=com.westworld.mafia.shooting.game&hl=en&pli=1",
       imgUrl: cowboyShootingcover,
       images: [
         CowboyShooting1,
@@ -181,27 +268,81 @@ export const Projects = () => {
         CowboyShooting6,
       ],
     },
+    {
+      title: "Windy Paratroopers (Mobile Game)",
+      description: "Windy Paratroopers is a fun and challenging arcade game where you guide paratroopers to safe landings while battling unpredictable winds. Time your jumps, adjust for wind speed, and avoid obstacles to ensure a smooth touchdown.",
+      link: "https://play.google.com/store/apps/details?id=com.tinykrakengames.windy.paratroppers",
+      imgUrl: Paratroopers0,
+      images: [
+        Paratroopers1,
+        Paratroopers2,
+        Paratroopers3,
+        Paratroopers4,
+        ParatroopersVideo,
+      ],
+    },
+    {
+      title: "Make Way (Mobile Game)",
+      description: "Make Way is a creative puzzle game where you build paths to guide objects safely to their destination. Arrange tiles, avoid obstacles, and solve tricky challenges across multiple levels. With intuitive controls and engaging mechanics, it's a fun test of logic and strategy!",
+      link: "https://play.google.com/store/apps/details?id=com.tinykrakengames.make.way",
+      imgUrl: MakeWay0,
+      images: [
+        MakeWay1,
+        MakeWay2,
+        MakeWay3,
+        MakeWayVideo,
+        MakeWayRep1,
+        MakeWayRep2,
+        MakeWayRep3,
+        MakeWayRep4,
+        MakeWayRep5,
+      ],
+    },
+    {
+      title: "Don't Touch - Escape Prison (Mobile Game)",
+      description: "Don't Touch It! is a casual arcade game where players must protect their precious object from waves of mischievous hands trying to steal it. Using quick reflexes and strategic taps, you fend off intruders while unlocking new defenses and upgrades. Simple yet addictive, it’s a fun challenge that keeps you on your toes!",
+      link: "https://play.google.com/store/apps/details?id=com.tinykrakengames.dont.touch.it&hl=en",
+      imgUrl: DontTouchIcon,
+      images: [
+        DontTouch1,
+        DontTouch2,
+        DontTouch3,
+        DontTouch4,
+        DontTouchVideo,
+      ],
+    },
+    {
+      title: "US Police Dog City Crime Chase (Mobile Game)",
+      description: "Police Dog Crime Chase Game is an action-packed simulation where you play as a police dog assisting officers in catching criminals. Patrol shopping malls, track down thieves, and stop illegal activities using your sharp senses and agility. With realistic K9 gameplay and thrilling chases, this game delivers an exciting law enforcement experience!",
+      link: "https://play.google.com/store/apps/details?id=com.gss.us.police.cop.dog.crime.chase.shoppingmall&hl=en",
+      imgUrl: USPoliceDogCityCrimeChase1,
+      images: [
+        USPoliceDogCityCrimeChase1,
+        USPoliceDogCityCrimeChase2,
+        USPoliceDogCityCrimeChase3,
+      ],
+    },
   ];
 
   const documents = [
     {
       title: "Save Load Data UML",
-      description: "",
+      description: "The SaveLoadData class manages game progress by storing and retrieving data using a unique save key. It tracks player-related information, such as selected character type, current scene, and position across different regions, all stored in GameData. This system ensures that task progress, player location, and other critical gameplay elements persist across sessions. Methods for saving and loading both player position and task data allow seamless game state restoration.",
       imgUrl: [saveLoadDataUML],
     },
     {
       title: "Interaction System UML",
-      description: "",
+      description: "Outlines the mechanics of object and NPC interaction, including proximity detection, UI indicators, and triggered events.",
       imgUrl: [interactionSystemUML],
     },
     {
       title: "Haywire Robot UML",
-      description: "",
+      description: "Details the behaviors of the haywire robot, including movement patterns, chase logic, and player interaction.",
       imgUrl: [haywireRobotUML],
     },
     {
       title: "Save Load Data DFD",
-      description: "",
+      description: "A Data Flow Diagram (DFD) for the save/load system, detailing how game data is stored, retrieved, and managed.",
       imgUrl: [saveLoadDataDFD],
     },
     {
@@ -231,32 +372,32 @@ export const Projects = () => {
     },
     {
       title: "Cyberpal Shooting System UML",
-      description: "",
+      description: "Represents the mechanics of shooting using the Cyberpal device, including input handling, projectile spawning, and hit detection.",
       imgUrl: [cyberpalShootingSystemUML],
     },
     {
       title: "Dialogue System UML",
-      description: "",
+      description: "Shows the structure of the dialogue system, including conversation flow, choice-based interactions, and NPC communication",
       imgUrl: [dialogueSystemUML],
     },
     {
       title: "State Machine UML",
-      description: "",
+      description: "Shows the player state system, likely handling movement, aiming, attacking, and other gameplay states.",
       imgUrl: [stateMachineUML],
     },
     {
       title: "Missions Handler UML",
-      description: "",
+      description: "Represents the mission system, including tracking objectives, updating mission status, and handling mission-related events.",
       imgUrl: [missionsHandlerUML],
     },
     {
       title: "Shooting System UML",
-      description: "",
+      description: "The Cyberpal Shooting System revolves around combat interactions with different enemy types, including Haywire robots and Virus enemies. The EnemyDetection component detects when a player is nearby, triggering appropriate behaviors. Players engage with enemies using the CyberpalShootingWidget, which manages aiming, shooting, and equip states. The system integrates with a PlayerStateMachine, ensuring smooth transitions between combat states. Events are managed through BoolEventChannelSO, facilitating dynamic interactions between components, while the AimUIHandler provides real-time aiming feedback.",
       imgUrl: [shootingsystemUML],
     },
     {
       title: "HUD UML",
-      description: "",
+      description: "Defines the structure of the Heads-Up Display (HUD), including elements like health bars, minimaps, notifications, and interaction prompts.",
       imgUrl: [HUDUML],
     },
   ];
